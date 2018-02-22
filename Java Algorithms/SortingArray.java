787fca877a6fdd393f701dac47104ea5fabda50d
Yamatoberu <gregory6267@gmail.com>
2018-02-22 12:11:27 -0700

    1st Sorting Algorithm Finished

--- Java Algorithms/SortingArray.java
+++ Java Algorithms/SortingArray.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
 import java.lang.Math;
+import java.time.LocalDateTime;
 
 class SortingArray{
 	public static void main(String[] args){
 		// Creating the array that will be sorted\
-		ArrayList randomArray = new ArrayList();
+		ArrayList<Integer> randomArray = new ArrayList();
 		randomArray.add(5);
 		randomArray.add(3);
 		randomArray.add(2);
@@ -12,32 +13,43 @@ class SortingArray{
 		randomArray.add(9);
 		randomArray.add(12);
 		
+		// Displaying the old array
+		System.out.println("Randomized Array: " + randomArray.toString());
+		
+		// Getting the start time
+		LocalDateTime startTime = LocalDateTime.now();
+		
 		// Creating the new ArrayList that will be willed with the sorted value
-		ArrayList newArray = new ArrayList();
+		ArrayList<Integer> newArray = new ArrayList();
 		
 		// Placing the first item into the New Array
 		newArray.add(randomArray.get(0));
-		int randLength = randomArray.size();
-		for(int randomCount = 1; randomCount < randLength; randomCount++){
-			boolean found = false;
-			int newLength = newArray.size();
-			System.out.println("New Array Size: " + newLength);
-			for(int newCount = 0; newCount < newLength; newCount++){
-				int check1 = (int) randomArray.get(randomCount);
-				int check2 = (int) newArray.get(newCount);
-				System.out.println("Check 1: " + check1);
-				System.out.println("Check 2: " + check2);
-				if(check1 <= check2){
-					newArray.add(newCount,randomArray.get(randomCount));
-					System.out.println("TRUE");
-					found = true;
+		
+		// Setting boolean to show if the item needs to be placed at the end of the array
+		boolean isFound = false;
+		
+		for(int randomCount = 1; randomCount < randomArray.size(); randomCount++){
+			isFound = false;
+//			System.out.println("Random Value: " + randomArray.get(randomCount));
+			for(int newCounter = 0; newCounter < newArray.size(); newCounter++){
+//				System.out.println("New Value: " + newArray.get(newCounter));
+				if (newArray.get(newCounter) >= randomArray.get(randomCount)){
+					newArray.add(newCounter,randomArray.get(randomCount));
+					isFound = true;
+					break;
 				}
 			}
-			if (found = false){
+			if (!isFound){
 				newArray.add(randomArray.get(randomCount));
 			}
 		}
+		System.out.println("New Array: " + newArray.toString());
+		
+		// Getting the end time
+		LocalDateTime endTime = LocalDateTime.now();
+		
+		// Displaying the duration
+		System.out.println("Duration: " + (endTime.getNano() - startTime.getNano())/1000000 + " milliseconds");
 	}
 }
-
 //WHY ISN'T NEWCOUNT ITERATING?!?!?!?!?!?!?!?!??!?!
